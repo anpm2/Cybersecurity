2c2dbf9e28b6cfd798fcdbbe86fd1339442dd410
Cyan <147069736+anpm2@users.noreply.github.com>
2024-08-17 20:22:47 +0700

    Create hello_word-linux.asm

---
+++ Reverse_Engineering/task/week1/hello_word-linux.asm
@@ -0,0 +1,18 @@
+;code on linux-32bit by Cyman
+
+section .data               ; khai báo dữ liệu khởi tạo, hằng
+    msg         db      'Hello wordl!', 0
+    len_msg     equ $ - msg ;độ dài chuỗi
+
+section .text               ;nơi thực thi dòng lệnh
+    global _start           ;khai báo nơi chương trình
+
+_start:                     ;cho biết điểm đầu vào của chương trình
+    mov     edx,    len_msg ;gán độ dài chuỗi vào edx
+    mov     ecx,    msg     ;gán chuỗi vào ecx
+    mov     ebx,    1       ;file mô tả (1 ~ stdout)
+    mov     eax,    4       ;gọi hệ thống (4 ~ sys_write) 
+    int     80h             ;gọi kernel để ngắt
+    
+    mov     eax,    1       ;gọi hệ thống (1 ~ sts_exit)
+    int 80h
