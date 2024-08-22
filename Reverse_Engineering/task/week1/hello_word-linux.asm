131304b53164a8c4b4bcbbe7801778e7b2bbc2db
Cyan <147069736+anpm2@users.noreply.github.com>
2024-08-22 23:35:25 +0700

    Update hello_word-linux.asm

--- Reverse_Engineering/task/week1/hello_word-linux.asm
+++ Reverse_Engineering/task/week1/hello_word-linux.asm
@@ -1,18 +1,18 @@
 ;code on linux_x86 by Cyman
 
 section .data               ; khai báo dữ liệu khởi tạo, hằng
-    msg         db      'Hello wordl!', 0
-    len_msg     equ $ - msg ;độ dài chuỗi
+    msg     db  'Hello wordl!', 0
+    len_msg equ $ - msg     ;độ dài chuỗi
 
 section .text               ;nơi thực thi dòng lệnh
     global _start           ;báo cho hệ điều hành biết nơi chương trình có thể được 
                             ;tìm thấy và thực thi
                             
 _start:                     ;cho biết điểm đầu vào của chương trình
-    mov     edx,    len_msg ;gán địa chỉ của độ dài chuỗi vào edx
-    mov     ecx,    msg     ;gán địa chỉ của chuỗi vào ecx
-    mov     ebx,    1       ;file descriptor (stdout) --> Đầu ra chuẩn (màn hình)
     mov     eax,    4       ;gọi hệ thống (sys_write) 
+    mov     ebx,    1       ;file descriptor (stdout) --> Đầu ra chuẩn (màn hình)
+    mov     ecx,    msg     ;gán địa chỉ của chuỗi vào ecx
+    mov     edx,    len_msg ;gán địa chỉ của độ dài chuỗi vào edx
     int     0x80            ;gọi kernel để ngắt
     
     mov     eax,    1       ;gọi hệ thống (sys_exit) 
